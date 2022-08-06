3a7bd46415706cc29ddebd5808be7e8007487c5e
LeroyK <785632486@qq.com>
2022-08-06 23:32:15 +0800

    2022年8月6日

--- 代码实现算法/two_sum.py
+++ 代码实现算法/two_sum.py
@@ -1,13 +1,13 @@
 # 两数之和
-sums = [range(10)]
+sums = range(10)
 target = 17
 
 
 def tow_sum():
     for i in sums:
         for x in sums:
-            if i != x and x * i == target:
-                print(i, "*", x, "=", target)
+            if i != x and x + i == target:
+                print(i, "+", x, "=", target)
 
 
 if __name__ == '__main__':
