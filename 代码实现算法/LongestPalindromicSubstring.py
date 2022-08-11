700efe322b8e6c11f09f1a2e5418881cb2c9c63b
LeroyK <785632486@qq.com>
2022-08-11 15:00:11 +0800

    2022年8月11日

--- 代码实现算法/LongestPalindromicSubstring.py
+++ 代码实现算法/LongestPalindromicSubstring.py
@@ -2,69 +2,56 @@
 # -*- coding: utf-8 -*-
 """
 找相同字符包裹的最长子串。
-以下过程还可以继续优化，我就不做探究了.
 """
 
+itemStr = {}
+sameCharacter = []
+res = []
 
-def demo(strs: str) -> str:
-    maxStr = ""
-    itemStr = ""
-    for i in strs:
-        flag = 1
 
-        # 直接把次数小于1的剔除掉
-        if strs.count(i) <= 1:
+def demo2(strs, index, el, maxStr=""):
+    seond_index = strs.find(el, (index + 1))
+    if seond_index == -1:
+        for value in itemStr[el]:
+            # 把最长那组返回回来
+            if len(value) > len(maxStr):
+                maxStr = value
+                s = (maxStr, el, len(maxStr))
+        return s
+
+    itemStr[el].append((strs[index:seond_index] + el))
+    return demo2(strs, seond_index, el)
+
+
+def demo(strs: str):
+    for index, el in enumerate(strs):
+        if strs.count(el) == 1:
+            sameCharacter.append(el)
             continue
+        if el not in sameCharacter:
+            itemStr[str(el)] = []
+            result = demo2(strs, index, el)
+            res.append(result)
+            sameCharacter.append(el)
+    # [('abssba', 'a', 6), ('bssb', 'b', 4), ('ss', 's', 2)]
+    # return res
+    """
+    写个冒泡算法，计算完毕
+    """
 
-        data = strs.split(i)
+    b = 0
+    a = res[b][2]
+    for i, value in enumerate(res):
+        if a < value[2]:
+            a = value[2]
+            b = i
 
+    done = res[b][0]
 
-        
-        if data[0] == "":
-            flag = 0
-        if data[-1] == "":
-            flag = -1
-        if data[-1] == data[0] == "":
-            flag = 2
-
-        itemStr = data[0]
-
-        for item in data:
-            if len(item) > len(itemStr):
-                """
-                判断是不是真的处于i + item + i 之间
-                """
-                match flag:
-                    case 1:
-                        # 去掉首尾两组元素
-                        if item == data[-1] or item == data[0]:
-                            pass
-                        else:
-                            itemStr = i + item + i
-                    case 0:
-                        # 去掉尾元素
-                        if item == data[-1]:
-                            pass
-                        else:
-                            itemStr = i + item + i
-                    case -1:
-                        # 去掉首元素
-                        if item == data[0]:
-                            pass
-                        else:
-                            itemStr = i + item + i
-                    case 2:
-                        # 
-                        itemStr = i + item + i
-
-
-        if len(maxStr) < len(itemStr):
-            maxStr = itemStr
-
-    return maxStr
+    return done
 
 
 if __name__ == '__main__':
-    strs = "sdlfjlkwjriuvnnvmzkwwqro"
-    maxStr = demo(strs)
-    print(maxStr)
+    strs = "abssbaasdfsadewwqweiorkldiufwqenebiuahjfwqoiuvhbaket"
+    done = demo(strs)
+    print(done)
