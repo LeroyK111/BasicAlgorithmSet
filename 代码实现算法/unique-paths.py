1c99c26b1e1f1a43cafe2efd97ed0ec1597be0fb
LeroyK <785632486@qq.com>
2022-12-24 15:56:16 +0800

    1

--- 代码实现算法/unique-paths.py
+++ 代码实现算法/unique-paths.py
@@ -4,11 +4,15 @@
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+
         dp = [[0 for _ in range(n)] for _ in range(m)]
+
         for i in range(m):
             dp[i][0] = 1
+
         for i in range(n):
             dp[0][i] = 1
+
         for i in range(1, m):
             for ii in range(1, n):
                 dp[i][ii] = dp[i - 1][ii] + dp[i][ii - 1]
