e4755e0810bbf5a1f9c0d3d8394c916b9ffaa479
LeroyK <bridieiybnf@gmail.com>
2025-06-03 21:58:10 +0800

    1

--- 代码实现算法/快速排序.py
+++ 代码实现算法/快速排序.py
@@ -3,12 +3,14 @@
 重新排序数列，所有元素比基准值小的摆放在基准前面，所有元素比基准值大的摆在基准的后面（相同的数可以到任一边）。在这个分区退出之后，该基准就处于数列的中间位置。这个称为分区（partition）操作；
 递归地（recursive）把小于基准值元素的子数列和大于基准值元素的子数列排序；
 
-
-
 """
+
+from torch import le
+
 
 def quickSort(arr, left=None, right=None):
     # 分割列表的索引值
+    # isinstance判断父子类
     left = 0 if not isinstance(left, (int, float)) else left
     right = len(arr) - 1 if not isinstance(right, (int, float)) else right
     if left < right:
